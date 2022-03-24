8da98d14d278f8c8161a29be27b8d5117d22985f
mve <miguel.valiente@quark-gluon.es>
2022-03-24 14:12:36 +0100

    fix workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/my-element.ts',
-      formats: ['es']
-    },
-    rollupOptions: {
-      external: /^lit/
-    }
-  }
+    
+  base: 'https://miguelve69.github.io/',
+  plugins: [react()]
 })
